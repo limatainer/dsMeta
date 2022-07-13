99dfa369451cc461b17b1e6c779b51d078b2970e
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2022-07-13 21:14:26 +0100

    Other components

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import icon from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+export default function Notification() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="" />
+    </div>
+  )
+}
